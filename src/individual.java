22ab1139f2e5bc0a6f3f02ef52692620f360a02a
stonelevi9 <61124534+stonelevi9@users.noreply.github.com>
2023-04-09 19:21:22 -0400

    Add files via upload

---
+++ src/individual.java
@@ -0,0 +1,14 @@
+public class individual {
+	String values;
+	int fitness;
+	boolean moved;
+	public individual() {
+		
+	}
+	public individual(String values, int fitness, boolean moved) {
+		this.values = values;
+		this.moved = moved;
+		this.fitness = fitness;
+	}
+
+}
---
+++ src/population.java
@@ -0,0 +1,18 @@
+
+public class population {
+	int popSize;
+	individual[] indiv = new individual[30];
+	String target;
+	public population() {
+		
+	}
+	public population(int popSize, individual[] indiv, String target) {
+		this.popSize = popSize;
+		this.indiv = indiv;
+		this.target = target;
+	}
+	
+	
+	
+
+}
